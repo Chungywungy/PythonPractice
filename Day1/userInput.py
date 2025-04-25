bf9d232bef972be99bfbe945c89a853ce9cf4a80
Chungywungy <placeholder0254@gmail.com>
2025-04-25 16:35:48 -0700

    user input

---
+++ Day1/userInput.py
@@ -0,0 +1,10 @@
+name = input("what's your name?")
+age = int(input("How old are you?"))
+hobby = input("What's your favourite hobby?")
+
+print("Hey", name, "you are", age, "years old and you like", hobby)
+
+number_1 = int(input("give me a number: "))
+number_2 = int(input("give me another number: "))
+
+print(number_1, "+",number_2, "=", number_1+number_2)
