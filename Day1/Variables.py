a439bc3b0153242119b7b3c24141e3a7b0cffb9e
Chungywungy <placeholder0254@gmail.com>
2025-04-25 16:24:28 -0700

    variables

---
+++ Day1/Variables.py
@@ -0,0 +1,9 @@
+name = "Chungywungy"
+age = 232224
+pi = 3.14159265
+is_real = True
+
+print("My name is", name)
+print("I am", age, "years old!")
+print("I know some numbers in pi",pi)
+print("I'm also a real human. Proof:",is_real)
