6195080051b50f8679db464d065e4588d2428ec4
Chungywungy <placeholder0254@gmail.com>
2025-04-25 22:39:58 -0700

    numberchecker

---
+++ Day1/NumberChecker.py
@@ -0,0 +1,8 @@
+num = int(input("Give me a number!!! NOW: "))
+
+if num > 100:
+    print("WOW that's huge :D")
+elif num > 50:
+    print("wow that's pretty average -_-")
+else: 
+    print("is that even worth talking about? yikes")
